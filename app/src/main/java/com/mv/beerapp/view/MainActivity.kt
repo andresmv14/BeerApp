866a33f3505f91944ac2bc8cea1a92b6c072fd8b
Andres <andres.mv14@outlook.com>
2022-06-25 11:28:41 -0500

    first commit

---
+++ app/src/main/java/com/mv/beerapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mv.beerapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mv.beerapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
